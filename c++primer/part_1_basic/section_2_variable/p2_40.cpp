88caf52cd1060f5200fad67f9434c15fda75af52
moyangcitta <2033402740@qq.com>
2022-03-26 15:55:24 +0800

    section2

---
+++ c++primer/part_1_basic/section_2_variable/p2_40.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+struct Sales_data{
+    std::string book_ISBN;
+    double sum_sales = 0.0;
+    int num = 0;
+    double single_sales = 0.0;
+};
+
+int main(){
+
+}
