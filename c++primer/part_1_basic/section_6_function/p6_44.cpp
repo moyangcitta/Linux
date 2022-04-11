6d04deaba007e3b0a6c0a1db39e6af214931074d
moyangcitta <2033402740@qq.com>
2022-04-11 17:34:14 +0800

    section_6 III

---
+++ c++primer/part_1_basic/section_6_function/p6_44.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+inline bool
+isShorter(const std::string &s1, const std::string &s2){
+    return s1.size() < s2.size();
+}
+constexpr bool Isshort(std::string &s1, std::string &s2){
+    return s1.size() < s2.size();
+}
+int main(){
+    
+    return 0;
+} 
